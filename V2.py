e763be463f88b20ee6d9178df9283616ddc9956e
FelipeFrug <felipe.frug.mauro@gmail.com>
2022-05-23 16:55:54 -0300

    ajuste da imagem

--- V2.py
+++ V2.py
@@ -12,11 +12,11 @@ pygame.display.set_caption('Jogo')
 
 
 # ----- definição tamanhos das estruturas
-altura_inicial_do_obstaculo = 50
-largura_inicial_do_obstaculo = 50
+altura_inicial_do_carro_da_fgv = 50
+largura_inicial_do_carro_da_fgv = 70
 
-obstaculo_img = pygame.image.load('recursos/imagem_do_meteoro.png')
-obstaculo_img_small = pygame.transform.scale(obstaculo_img, (largura_inicial_do_obstaculo, altura_inicial_do_obstaculo))
+carro_da_fgv_img = pygame.image.load('recursos/imagem_do_carro_da_fgv.png').convert_alpha()
+carro_da_fgv_img_small = pygame.transform.scale(carro_da_fgv_img, (largura_inicial_do_carro_da_fgv, altura_inicial_do_carro_da_fgv))
 
 
 
@@ -25,11 +25,11 @@ game = True
 
 # ----- Posição do obstáculo
 
-posição_x_obstaculo = 200
-posição_y_obstaculo = -altura_inicial_do_obstaculo
+posição_x_carro_da_fgv = 200
+posição_y_carro_da_fgv = -altura_inicial_do_carro_da_fgv
 
-velocidade_x_do_obstaculo = -0.015
-velocidade_y_do_obstaculo = 0.05
+velocidade_x_do_carro_da_fgv = -0.015
+velocidade_y_do_carro_da_fgv = 0.05
 
 # ===== Loop principal =====
 while game:
@@ -41,16 +41,16 @@ while game:
 
     # ----- Atualiza estado do jogo
     # Atualizando a posição do meteoro
-    posição_x_obstaculo += velocidade_x_do_obstaculo
-    posição_y_obstaculo += velocidade_y_do_obstaculo
+    posição_x_carro_da_fgv += velocidade_x_do_carro_da_fgv
+    posição_y_carro_da_fgv += velocidade_y_do_carro_da_fgv
     # Se o obstaculo passar do final da tela, volta para cima
-    if posição_y_obstaculo > altura_da_tela or posição_x_obstaculo + largura_inicial_do_obstaculo < 0 or posição_x_obstaculo > largura_da_tela:
-        posição_x_obstaculo = 200
-        posição_y_obstaculo = -altura_inicial_do_obstaculo
+    if posição_y_carro_da_fgv > altura_da_tela or posição_x_carro_da_fgv + largura_inicial_do_carro_da_fgv < 0 or posição_x_carro_da_fgv > largura_da_tela:
+        posição_x_carro_da_fgv = 200
+        posição_y_carro_da_fgv = -altura_inicial_do_carro_da_fgv
 
     # ----- Gera saídas
-    window.fill((255, 255, 255))  # Preenche com a cor branca
-    window.blit(obstaculo_img_small, (posição_x_obstaculo, posição_y_obstaculo))
+    window.fill((255, 255, 0))  # Preenche com a cor branca
+    window.blit(carro_da_fgv_img_small, (posição_x_carro_da_fgv, posição_y_carro_da_fgv))
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
