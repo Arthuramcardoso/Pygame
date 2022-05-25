60475449c0007416a696f0bf42185f83c4b6ea56
FelipeFrug <felipe.frug.mauro@gmail.com>
2022-05-25 15:25:08 -0300

    adicionando mais obstaculos

--- V3.py
+++ V3.py
@@ -5,25 +5,39 @@ import pygame
 pygame.init()
 
 # ----- Gera tela principal
-altura_da_tela = 720
-largura_da_tela = 1270
+altura_da_tela = 500
+largura_da_tela = 1000
 window = pygame.display.set_mode((largura_da_tela, altura_da_tela))
 pygame.display.set_caption('Jogo')
 
 
-# ----- definição tamanhos das estruturas
+# ----- definição tamanhos e propriedades das estruturas
 
-altura_inicial_dos_obstaculos = 160
-largura_inicial_dos_obstaculos = 240
+altura_inicial_dos_obstaculos = 120
+largura_inicial_dos_obstaculos = 180
+
+velocidade_x_dos_obstaculos = 0
+velocidade_y_dos_obstaculos = 8
+
+tela_de_fundo_img = pygame.image.load('recursos/fundo.png').convert()
+tela_de_fundo_img = pygame.transform.scale(tela_de_fundo_img,(largura_da_tela, altura_da_tela))
 
 carro_da_fgv_img = pygame.image.load('recursos/imagem_do_carro_da_fgv.png').convert_alpha()
 carro_da_fgv_img = pygame.transform.scale(carro_da_fgv_img, (largura_inicial_dos_obstaculos, altura_inicial_dos_obstaculos))
 
-carro_do_marcão_img = pygame.image.load('recursos/imagem_do_carro_do_marcão.png').convert_alpha()
-carro_do_marcão_img = pygame.transform.scale(carro_do_marcão_img,(largura_inicial_dos_obstaculos, altura_inicial_dos_obstaculos))
+carro_do_marcao_img = pygame.image.load('recursos/imagem_do_carro_do_marcão.png').convert_alpha()
+carro_do_marcao_img = pygame.transform.scale(carro_do_marcao_img,(largura_inicial_dos_obstaculos, altura_inicial_dos_obstaculos))
+
+carro_da_espm_img = pygame.image.load('recursos/imagem_do_carro_da_espm.png').convert_alpha()
+carro_da_espm_img = pygame.transform.scale(carro_da_espm_img,(largura_inicial_dos_obstaculos, altura_inicial_dos_obstaculos))
 
 # ----- Posições e velocidades iniciais
-esquerda = [175, -altura_inicial_dos_obstaculos, 0, 8]
+muito_esquerda = [0, -altura_inicial_dos_obstaculos, velocidade_x_dos_obstaculos, velocidade_y_dos_obstaculos]
+esquerda = [200, -altura_inicial_dos_obstaculos, velocidade_x_dos_obstaculos, velocidade_y_dos_obstaculos]
+meio = [400, -altura_inicial_dos_obstaculos, velocidade_x_dos_obstaculos, velocidade_y_dos_obstaculos]
+direita = [600, -altura_inicial_dos_obstaculos, velocidade_x_dos_obstaculos, velocidade_y_dos_obstaculos]
+muito_direita = [800 - largura_inicial_dos_obstaculos, -altura_inicial_dos_obstaculos, velocidade_x_dos_obstaculos, velocidade_y_dos_obstaculos]
+
 
 
 # ----- Inicia estruturas de dados
@@ -64,7 +78,9 @@ FPS = 20
 
 # ----- Criando obstaculos
 
-carro_da_fgv = Obstaculo(carro_da_fgv_img,esquerda)
+carro_da_fgv = Obstaculo(carro_da_fgv_img, esquerda)
+carro_do_marcao = Obstaculo(carro_do_marcao_img, meio)
+carro_da_espm = Obstaculo(carro_da_espm_img, direita)
 
 # ===== Loop principal =====
 while game:
@@ -79,11 +95,17 @@ while game:
     # ----- Atualiza estado do jogo
 
     carro_da_fgv.update()
+    carro_do_marcao.update()
+    carro_da_espm.update()
+
 
     # ----- Gera saídas
     window.fill((255, 255, 255))  # Preenche com a cor branca
+    window.blit(tela_de_fundo_img,(0,0))
 
-    window.blit(carro_da_fgv.image, (carro_da_fgv.rect.x, carro_da_fgv.rect.y))
+    window.blit(carro_da_fgv.image, carro_da_fgv.rect)
+    window.blit(carro_da_espm.image, carro_da_espm.rect)
+    window.blit(carro_do_marcao.image, carro_do_marcao.rect)
 
     # ----- Atualiza estado do jogo
     pygame.display.update()  # Mostra o novo frame para o jogador
